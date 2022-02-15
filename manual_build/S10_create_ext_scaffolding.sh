57c0474a06d7d4b543e7e6943fe9eb921b23c67a
Swanny <60895883+neilswann80@users.noreply.github.com>
2022-02-15 06:42:42 +0000

    simplify with case function
    Yep, you're definitely correct, far neater.  I wish Windows batch had that function!

--- manual_build/S10_create_ext_scaffolding.sh
+++ manual_build/S10_create_ext_scaffolding.sh
@@ -11,50 +11,35 @@ then
       fi
     done
 
-    for dir in roms/ARCADE roms/CPS roms/FBA roms/DAPHNE roms/FC    \
-        roms/SFC roms/VB roms/SG1000 roms/SMS roms/MD roms/SEGACD   \
-        roms/32X roms/A2600 roms/A5200 roms/A7800 roms/INTELLI      \
-        roms/COLECO roms/NEOGEO roms/PCE roms/PCECD roms/PS roms/GB \
-        roms/GBC roms/GBA roms/GW roms/GG roms/LYNX roms/NGP        \
-        roms/WSC roms/POKEMINI roms/SUPERVISION roms/ZX             \
-        roms/AMSTRAD roms/C64 roms/MSX roms/AMIGA roms/DOOM/DOOM    \
-        roms/DOOM/DOOM2 roms/DOSBOX roms/SCUMMVM roms/PICO8         \
-        roms/TIC80 roms/BBCMICRO roms/FDS roms/SGB roms/QUAKE/id1   \
-        roms/QUAKE/hipnotic roms/QUAKE/rogue roms/QUAKE/dopa        \
-        roms/QUAKE2/baseq2 roms/QUAKE2/rogue roms/QUAKE2/xatrix     \
-        roms/QUAKE2/zaero
+    for dir in ARCADE CPS FBA DAPHNE FC SFC VB SG1000 SMS MD SEGACD \
+        32X A2600 A5200 A7800 INTELLI COLECO NEOGEO PCE PCECD PS GB \
+        GBC GBA GW GG LYNX NGP WSC POKEMINI SUPERVISION ZX AMSTRAD  \
+        C64 MSX AMIGA DOOM/DOOM DOOM/DOOM2 DOSBOX SCUMMVM PICO8     \
+        TIC80 BBCMICRO FDS SGB QUAKE/id1 QUAKE/hipnotic QUAKE/rogue \
+        64DD QUAKE/dopa QUAKE2/baseq2 QUAKE2/rogue QUAKE2/xatrix    \
+        QUAKE2/zaero
     do
-      if [ ! -d "/media/sdcard/${dir}/.previews" ]; then
-        mkdir -p "/media/sdcard/${dir}/.previews"
+      if [ ! -d "/media/sdcard/roms/${dir}/.previews" ]; then
+        mkdir -p "/media/sdcard/roms/${dir}/.previews"
       fi
     done
 
-
-    for dir in roms/QUAKE/id1 roms/QUAKE/hipnotic roms/QUAKE/rogue  \
-        roms/QUAKE/dopa roms/QUAKE2/baseq2 roms/QUAKE2/rogue        \
-        roms/QUAKE2/xatrix roms/QUAKE2/zaero
+    dir1="/media/sdcard/roms" && pak="exec=pak0.pak"
+    for dir2 in QUAKE/id1 QUAKE/hipnotic QUAKE/rogue QUAKE/dopa     \
+        QUAKE2/baseq2 QUAKE2/rogue QUAKE2/xatrix QUAKE2/zaero
     do
-      count=`ls -1 /media/sdcard/${dir}/*.fbl 2>/dev/null | wc -l`
-      if [ $count -eq 0 ] && [ -e "/media/sdcard/${dir}/pak0.pak" ]; then
-        # Quake launchers
-        if [ ${dir} = roms/QUAKE/id1 ]; then
-          echo "exec=pak0.pak" > "/media/sdcard/${dir}/Quake.fbl"
-        elif [ ${dir} = roms/QUAKE/hipnotic ]; then
-          echo "exec=pak0.pak" > "/media/sdcard/${dir}/Mission pack 1.fbl"
-        elif [ ${dir} = roms/QUAKE/rogue ]; then
-          echo "exec=pak0.pak" > "/media/sdcard/${dir}/Mission pack 2.fbl"
-        elif [ ${dir} = roms/QUAKE/dopa ]; then
-          echo "exec=pak0.pak" > "/media/sdcard/${dir}/Episode 5. Dimension of the Past.fbl"
-        # Quake 2 launchers
-        elif [ ${dir} = roms/QUAKE2/baseq2 ]; then
-          echo "exec=pak0.pak" > "/media/sdcard/${dir}/Quake II.fbl"
-        elif [ ${dir} = roms/QUAKE2/rogue ]; then
-          echo "exec=pak0.pak" > "/media/sdcard/${dir}/Ground Zero.fbl"
-        elif [ ${dir} = roms/QUAKE2/xatrix ]; then
-          echo "exec=pak0.pak" > "/media/sdcard/${dir}/The Reckoning.fbl"
-        elif [ ${dir} = roms/QUAKE2/zaero ]; then
-          echo "exec=pak0.pak" > "/media/sdcard/${dir}/Zaero.fbl"
-        fi
+      count=`ls -1 ${dir1}/${dir2}/*.fbl 2>/dev/null | wc -l`
+      if [ $count -eq 0 ] && [ -e "${dir1}/${dir2}/pak0.pak" ]; then
+        case ${dir2} in
+          QUAKE/id1)		echo "$pak" > "${dir1}/${dir2}/Quake.fbl" ;;
+          QUAKE/hipnotic)	echo "$pak" > "${dir1}/${dir2}/Mission pack 1.fbl" ;;
+          QUAKE/rogue)		echo "$pak" > "${dir1}/${dir2}/Mission pack 2.fbl" ;;
+          QUAKE/dopa)		echo "$pak" > "${dir1}/${dir2}/Episode 5. Dimension of the Past.fbl" ;;
+          QUAKE2/baseq2)	echo "$pak" > "${dir1}/${dir2}/Quake II.fbl" ;;
+          QUAKE2/rogue)	    echo "$pak" > "${dir1}/${dir2}/Ground Zero.fbl" ;;
+          QUAKE2/xatrix)	echo "$pak" > "${dir1}/${dir2}/The Reckoning.fbl" ;;
+          QUAKE2/zaero)	    echo "$pak" > "${dir1}/${dir2}/Zaero.fbl" ;;
+        esac
       fi
     done
 fi
